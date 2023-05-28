948096d0ae0c9c4e0f329a4719c7c64ed0beb6e0
ssun4098 <bebo007@naver.com>
2023-05-28 21:36:47 +0900

    feat 로그인 화면 개발, 로그인 API 연동

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,67 @@
 <template>
-<div>
-  <h1>Login</h1>
-</div>
+<form @submit="onSubmit">
+  <div class="mb-3">
+    <label for="input-loginId" class="form-label">ID</label>
+    <input type="text" id="input-loginId" class="form-control" v-model="form.loginId">
+  </div>
+
+  <div class="mb-3">
+    <label for="input-password" class="form-label">Password</label>
+    <input type="password" id="input-password" class="form-control" v-model="form.password">
+  </div>
+
+  <button type="submit" class="btn btn-primary">로그인</button>
+</form>
 </template>
 
 <script>
 export default {
-  name: "MyLogin"
+  name: "MyLogin",
+  data() {
+    return {
+      form: {
+        loginId: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+
+      if(this.checkLoginId()) {
+        alert('로그인 아이디를 입력해주세요');
+        return;
+      }
+
+      if(this.form.password === '') {
+        alert('비밀번호를 입력해주세요.');
+        return;
+      }
+
+      this.$axios({
+        method: "post",
+        url: "http://localhost:8080/v1/members/login",
+        data: JSON.stringify(this.form),
+        headers: {
+          "Content-Type" : "application/json; charset=utf-8"
+        },
+        timeout: 5000
+      })
+          .then(function () {
+            alert("Login Success");
+          })
+          .catch(function (error) {
+            alert(error);
+      })
+    },
+    checkLoginId() {
+      return this.form.loginId === '';
+    },
+    checkPassword() {
+      return this.form.password === '';
+    }
+  }
 }
 </script>
 
